82ec6ecceae5cadd7a876ba1661821477be676d1
Paul Thompson <mailto.prt@gmail.com>
2018-02-09 10:37:50 -0500

    added display functions

--- Token.cpp
+++ Token.cpp
@@ -4,93 +4,285 @@
 #include <cassert>
 #include "Token.hpp"
 
-//TODO:
-/* define all helper functions: attr.get(), to_string()
-   switch(str)
-        case relational_operators:
-        return "relational-operator";
-*/
-
-// define attribute.get();
-
-bool
-Token::has_attribute()
-{
-        switch(name)
-        {
-                // list of tokens that need/have an attribute
-                case token_decimal_int_literal:
-                case token_binary_int_literal:
-                case token_hex_digit:
-                case token_hex_int_literal:
-                case token_binary_digit:
-                case token_floating_point_literal:
-                case token_boolean_literal:
-                case token_character_literal:
-                case token_string_literal:
-                case relational_operators:
-                case arithmatic_operators:
-                case bitwise_operators:
-                case logical_operators:
-                case type_specifier:
-                        return true;
-                default:
-                        return false;
-        }
-}
 
 //constructors
 Token::Token(token_name name, Location loc)
-        : name(name), location(loc);
+    : name(name), location(loc);
 {
-        assert(!has_attribute);
-
+    assert(!has_attribute());
 }
 
-Token::Token(const char* str)
-        :
-{
-        assert(!has_attribute);
+Token::Token(symbol symbol, Location loc)
+    : name(token_identifier),
+      attribute(symbol),
+      location(loc)
+{ }
+
+Token::Token(relational_operators rop, Location loc)
+    : name(token_relational_op), 
+      attr(rop),
+      location(loc)
+{ }
+
+Token::Token(arithmatic_operators aop, Location loc)
+    : name(token_arithmatic_op),
+      attr(aop),
+      location(loc)      
+{ }
+
+Token::Token(bitwise_operators bop, Location loc)
+    : name(token_bitwise_op),
+      attr(bop),
+      location(loc)
+{ }
+
+Token::Token(logical_operators lop, Location loc)
+    : name(token_logical_op),
+          attr(lop),
+          location(loc)
+{ }
+
+
+Token::Token(type_specifier ts, Location loc)
+    : name(token_type_specifier),
+          attr(ts),
+          location(loc)
+{ }
+
+Token::Token(double num, Location loc)
+    : name(token_floating_point_literal),
+      attr(num),
+      location(loc)
+{ }
+
+Token::Token(bool truefalse, Location loc)
+        : name(token_boolean_literal),
+          attr(truefalse),
+          location(loc)
+{ }
+
+const char* 
+display(token_name name){
+    switch(name){
+        case token_left_brace:
+            return "left brace";
+        case token_right_brace:
+            return "right brace";
+        case token_left_paren:
+            return "left paren";
+        case token_right_paren:
+            return "right paren";
+        case token_left_bracket:
+            return "left bracket";
+        case token_right_bracket:
+            return "right bracket";
+        case token_comma:
+            return "comma";
+        case token_semicolon:
+            return "semicolon";
+        case token_colon:
+            return "colon";
+        case token_conditional_op:
+            return "conditional operator";
+        case token_assignment_op:
+            return "assignment operator";
+        case token_relational_op:
+            return "relaptional operator";
+        case token_arithmatic_op:
+            return "arithmatic operator";
+        case token_bitwise_op:
+            return "bitwise operator";
+        case token_logical_op:
+            return "logical operator";
+        case token_type_specifier:
+            return "type specifier";
+        case token_keywords:
+            return "keyword";
+        case token_identifier:
+            return "identifier";
+        case token_decimal_int:
+            return "decimal integer";
+        case token_binary_int:
+            return "binary integer";
+        case token_hex_int:
+            return "hex integer";
+        case token_floating_point_literal:
+            return "floating point literal";
+        case token_boolean_literal:
+            return "boolean";
+        case token_character_literal:
+            return "character literal";
+        case token_string_literal:
+            return "string";
+    }
+}                  
+
+const char* 
+display(keywords keyword){
+    switch(keyword){
+        case key_def:
+            return "def";
+        case key_else:
+            return "else";
+        case key_if:
+            return "if";
+        case key_let:
+            return "let";
+        case key_var:
+            return "var";
+    }
 }
 
-Token::Token(relational_operators op)
-        :
-{
-
+const char* 
+display(relational_operators op){
+    switch(op){
+        case op_equals:
+            return "equals";
+        case op_notequal:
+            return "not equals";
+        case op_lessthan:
+            return "less than";
+        case op_greaterthan:
+            return "greater than";
+        case op_ltequals:
+            return "less than or equals";
+        case op_gtequals:
+            return "greater than or equals";
+    }
 }
 
-Token::Token(arithmatic_operators aop)
-        :
-{
-
+const char* 
+display(arithmatic_operators op){
+    switch(op){
+        case op_plus:
+            return "plus";
+        case op_minus:
+            return "minus";
+        case op_mult:
+            return "multiply";
+        case op_divide:
+            return "divide";
+        case op_ltequals:
+            return "modulo";
+    }
 }
 
-Token::Token(bitwise_operators bop)
-        :
-{
-
+const char* 
+display(bitwise_operators op){
+    switch(op){
+        case op_and:
+            return "&";
+        case op_or:
+            return "|";
+        case op_xor:
+            return "^";
+        case op_leftshift:
+            return "<<";
+        case op_rightshift:
+            return ">>";
+        case op_not:
+            return "~";
+    }
 }
 
-Token::Token(logical_operators lop)
-        :
-{
-
+const char* 
+display(logical_operators op){
+    switch(op){
+        case op_AND:
+            return "and";
+        case op_OR:
+            return "or";
+        case op_NOT:
+            return "not";  
+    }
 }
 
-Token::Token(type_specifier ts)
-        :
-{
-
+const char* 
+display(type_specifier type){
+    switch(op){
+        case ts_bool:
+            return "bool";
+        case ts_char:
+            return "char";
+        case ts_int:
+            return "int";
+        case ts_float:
+            return "float";
+        case ts_void:
+            return "void";
+    }
 }
 
-Token::Token(double num)
-        :
-{
-
+const char* 
+display(char* str){
+    return str;
 }
 
-Token::Token(float fp)
-        :
-{
+const char* 
+display(symbol symbol){
+    return symbol;
+}
 
+const char* 
+display(integer_token num){
+    return std::to_string(num.value)
+}
+
+const char* 
+display(float num){
+    // std::to_string can have problems with floats
+    return std::to_wstring(num);
+}
+
+const char*
+display(bool boolval){
+    return boolval ? "true" : "false";
+}
+
+const char* 
+display(location loc){
+    ostringstream ss;
+    ss << "line: " << loc.line;
+    ss << " column: " << loc.column;
+    ss << " in file: " << loc.file;
+    return ss.str();
+}
+
+const char
+display(char c){ return c; }
+
+bool
+Token::has_attribute(){
+    switch(name){
+        // list of tokens that need/have an attribute
+        case token_hex_int:
+        case token_binary_int:
+        case token_decimal_int:
+        case token_floating_point_literal:
+        case token_boolean_literal:
+        case token_character_literal:
+        case token_string_literal:
+        case token_identifier:
+        case relational_operators:
+        case arithmatic_operators:
+        case bitwise_operators:
+        case logical_operators:
+        case type_specifier:
+                return true;
+        default:
+                return false;
+    }
+}
+
+
+std::ostream& 
+operator << (std::ostream os, Token token){
+
+    os << '<';
+
+    if(!token.has_attribute()) return os << display(token.get_name()) << '>';
+
+    os << display(token.get_name()) << ': ';
+    os << display(token.get_attr()) << ">"; 
+    return os;
 }
--- Token.hpp
+++ Token.hpp
@@ -5,10 +5,10 @@
 #include "Token_enums.hpp"
 #include "Location.hpp"
 
-
 struct Token 
 {
     // one constructor for each broad type of token
+    Token() = default;
     Token(token_name name, Location loc);
     Token(const char* str, Location loc);
     Token(relational_operators op, Location loc);
@@ -24,19 +24,57 @@ struct Token
     // some comvienence. converts to true if not eof
     explicit operator bool() const { return name != tok_eof; }
 
-    // TODO: provide to_string method to print tokens according to spec:
-    // <token_name:token_attr>\n
-    template<typename T>
-    const std::string to_string(const T& val)
-    {
-        std::ostringstream ss;
-        ss << '<' << val << ':' << attr << '>';
-        return ss.str();
-    }
+    const char* display(token_name name);
+    const char* display(symbol symbol);
+    const char* display(keywords keyword);
+    const char* display(relational_operators op);
+    const char* display(arithmatic_operators op);
+    const char* display(bitwise_operators op);
+    const char* display(logical_operators op);
+    const char* display(type_specifier type);
+    const char* display(integer_token num);
+    const char* display(char* str);
+    const char* display(float num);
+    const char* display(bool boolval);
+
 
     void set_file_path(std::string file) {return location.set_path(file);}
     void init_location() {return location.init();}
-    bool has_attribute(){}
+    bool has_attribute();
+    token_name get_name() { return name; }
+
+    template<typename T>
+    T get_attr(){
+        assert(this.has_attribute());
+        
+        switch(name){
+            case token_hex_int:
+            case token_binary_int:
+            case token_decimal_int:
+                return attr.intval;
+            case token_floating_point_literal:
+                return attr.floatval;
+            case token_boolean_literal:
+                return attr.boolval;
+            case token_character_literal:
+                return attr.ch;
+            case token_string_literal:
+                return attr.str;
+            case token_identifier:
+                return attr.sym;
+            case relational_operators:
+                return attr.relop;
+            case arithmatic_operators:
+                return attr.arthop;
+            case bitwise_operators:
+                return attr.bitop;
+            case logical_operators:
+                return attr.logop;
+            case type_specifier:
+                return attr.typespec;
+        }
+     }
+
 
     private:
         token_name name;
@@ -44,30 +82,40 @@ struct Token
         location location;
 };
 
+struct integer_token {
+    radix radix;
+    long long value;
+}
+
+
 union attribute
 {
     attribute() = default;
     attribute(symbol s): sym(s){}
-    attribute(char* str) : str(str){}
-    attribute(bool truefalse) : boolval(truefalse) {}
+    attribute(keywords key) : keyword(key){}
     attribute(relational_operators op) : relop(op) {}
     attribute(arithmatic_operators op) : arthop(op) {}
     attribute(bitwise_operators op) : bitop(op) {}
     attribute(logical_operators op) :logop(op) {}
     attribute(type_specifier ts) : typespec(ts) {}
-    attribute(double num) : intval(n) {}
+    attribute(integer_token inttok) : intval(inttok) {}
     attribute(float fp) : floatval(fp) {}
+    attribute(char* str) : str(str){}
+    attribute(char c) : ch(c) {}
+    attribute(bool truefalse) : boolval(truefalse) {}
  
     // member vars for the token types
     symbol sym;
+    keyword key;
     relational_operators relop;
     arithmatic_operators arthop;
     bitwise_operators bitop;
     logical_operators logop;
     type_specifier typespec;
+    integer_token intval;
     bool boolval;
     char* str;
-    double intval;
+    char ch;
     float floatval;
 
 };
