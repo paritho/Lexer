359203aecc96e9febc9e4abd667097c639289b32
Paul Thompson <mailto.prt@gmail.com>
2018-02-09 11:17:58 -0500

    added set_path function

--- Location.hpp
+++ Location.hpp
@@ -4,13 +4,15 @@
 struct Location
 {
     Location(std::string filePath)
-        : file(filepath),line(0), column(0)
+        : file(filepath),line(-1), column(-1)
     {}
 
     ~Location() = default;
 
     void next_line() { return ++line; }
     void next_column(){ return ++column; }
+    void set_column(int col) {return column = col; }
+    void set_path(std::string path) { return file = path; }
 
     private:
         std::string file;
