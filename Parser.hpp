2db0fc532ec32ee3289570b50437f8566c2bff0f
Paul Thompson <mailto.prt@gmail.com>
2018-03-18 17:07:46 -0400

    added helper functions

--- Parser.hpp
+++ Parser.hpp
@@ -9,10 +9,15 @@ struct Parser{
     Parser(Symbol_table& syms, const std::string& source);
 
     void parse_type();
+    void parse_basic_type();
+    void parse_type_list();
+    void parse_postfix_type();
+    void parse_ref_type();
 
     // epxressions
     void parse_expr();
     void parse_assign_expr();
+    void parse_constant_expr();
     void parse_conditional_expr();
     void parse_log_or_expr();
     void parse_log_and_expr();
@@ -21,15 +26,23 @@ struct Parser{
     void parse_bit_xor_expr();
     void parse_equality_expr();
     void parse_relational_expr();
-    void parse_shift_expr_expr();
+    void parse_shift_expr();
     void parse_add_expr();
     void parse_mult_expr();
     void parse_cast_expr();
     void parse_unary_expr();
     void parse_postfix_expr();
+    void parse_arg_list();
+    void parse_argument();
     void parse_primary_expr();
 
+    // stmts
     void parse_stmt();
+    void parse_block_stmt();
+    void parse_if_stmt();
+    void parse_while_stmt();
+    void parse_stmtseq();
+    void parse_return_stmt();
 
     // declarations and definitions
     void parse_decl();
@@ -39,6 +52,8 @@ struct Parser{
     void parse_const_def();
     void parse_val_def();
     void parse_func_def();
+    void parse_param_list();
+    void parse_parameter();
     void parse_decl_seq();
 
     void parse_program();
