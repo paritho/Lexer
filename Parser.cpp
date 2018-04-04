53d8beb177debeae0ea5fdbf4d718dddc7d4c538
Paul Thompson <mailto.prt@gmail.com>
2018-04-03 20:46:23 -0400

    added return types

--- Parser.cpp
+++ Parser.cpp
@@ -11,14 +11,14 @@ Parser::Parser(Symbol_table& syms, const std::string& source)
 }
 
 // post type
-void
+Type*
 Parser::parse_type(){
   parse_postfix_type();
 }
 
 // void | bool | int | float | char
 // | ( type list? ) -> type | type
-void 
+Type* 
 Parser::parse_basic_type(){
     parse_type();
 
@@ -47,7 +47,7 @@ Parser::parse_basic_type(){
 }
 
 // type-list , type | type
-void 
+Type* 
 Parser::parse_type_list(){
     parse_basic_type();
     while(lookahead() == token_semicolon) parse_basic_type(); 
@@ -55,7 +55,7 @@ Parser::parse_type_list(){
 
 // post * | post const | post volatile | post [ expr ]
 // | post [] | basic type
-void
+Type*
 Parser::parse_postfix_type(){
     parse_basic_type();
 
@@ -71,20 +71,20 @@ Parser::parse_postfix_type(){
 }
 
 // ref type & | post type
-void 
+Type* 
 Parser::parse_ref_type(){
 
 }
 
 // assign expr
-void 
+Expr* 
 Parser::parse_expr(){
     parse_assign_expr();
 
 }
 
 // cond expr = assign expr | cond expr
-void 
+Expr*
 Parser::parse_assign_expr(){
     parse_conditional_expr();
     Token token = peek();
@@ -97,13 +97,13 @@ Parser::parse_assign_expr(){
   return;
 }
 
-void 
+Expr*
 Parser::parse_constant_expr(){
     parse_conditional_expr();
 }
 
 // log or expr ? expr : cond expr | log or expr
-void 
+Expr*
 Parser::parse_conditional_expr(){
     parse_log_or_expr();
 
@@ -121,14 +121,14 @@ Parser::parse_conditional_expr(){
 }
 
 // log or expr or log and expr | log and expr
-void 
+Expr* 
 Parser::parse_log_or_expr(){
     parse_log_and_expr();
     while(matchif_log_OR()) parse_log_and_expr();
 }
 
 // log and expr AND bit or expr | bit or expr
-void 
+Expr*
 Parser::parse_log_and_expr(){
     parse_bit_or_expr();
     while(matchif_log_AND()) parse_bit_or_expr();
@@ -136,28 +136,28 @@ Parser::parse_log_and_expr(){
 }
 
 // bit or expr OR xor expr | xor expr
-void 
+Expr*
 Parser::parse_bit_or_expr(){
     parse_bit_xor_expr();
     while(matchif_bit_or()) parse_bit_xor_expr(); 
 }
 
 // bit xor expr ^ bit and expr | bit and expr
-void 
+Expr*
 Parser::parse_bit_xor_expr(){
     parse_bit_and_expr();
     while(matchif_bit_xor()) parse_bit_and_expr();
 }
 
 // bit and expr & rel expr | rel expr
-void 
+Expr*
 Parser::parse_bit_and_expr(){
     parse_relational_expr();
     while(matchif_bit_and()) parse_relational_expr();
 }
 
 // eq expr == rel expr | eq != rel | rel expr
-void 
+Expr*
 Parser::parse_equality_expr(){
     parse_relational_expr();
     while(matchif_eq()) parse_relational_expr();
@@ -165,14 +165,14 @@ Parser::parse_equality_expr(){
 
 // rel expr < shift | rel > shift | rel <= shift
 // | rel >= shift | shift epxr
-void 
+Expr*
 Parser::parse_relational_expr(){
     parse_shift_expr();
     while(matchif_rel()) parse_shift_expr();
 }
 
 // shift expr << add expr | shift >> add | add expr
-void 
+Expr*
 Parser::parse_shift_expr(){
     parse_add_expr();
     while(matchif_shift()) parse_add_expr();
@@ -180,7 +180,7 @@ Parser::parse_shift_expr(){
 
 // add expr + mult expr | add expr - mult expr
 // | mult expr
-void 
+Expr*
 Parser::parse_add_expr(){
     parse_mult_expr();
     while(matchif_add()) parse_mult_expr();
@@ -189,14 +189,14 @@ Parser::parse_add_expr(){
 
 // mult expr * cast expr | mult / cast | mult % cast
 // | cast
-void 
+Expr*
 Parser::parse_mult_expr(){
     parse_cast_expr();
     while(matchif_mult()) parse_cast_expr();
 }
 
 // cast expr as type | unexpr
-void 
+Expr*
 Parser::parse_cast_expr(){
     parse_unary_expr();
 
@@ -204,14 +204,14 @@ Parser::parse_cast_expr(){
 
 // + unexpr | - unexpr | ~ unexpr | !unexpr (<-- this ! not in lang?)
 // & unexpr | * unexpr | postfix expr
-void 
+Expr* 
 Parser::parse_unary_expr(){
     parse_postfix_expr();
     while(matchif(token_unary_op)) parse_unary_expr();   
 }
 
 // postfix ( arg list ) | postfix [ arg list ] | prim expr
-void 
+Expr*
 Parser::parse_postfix_expr(){
     parse_primary_expr();
     switch(lookahead()){
@@ -234,19 +234,19 @@ Parser::parse_postfix_expr(){
 }
 
 // arg list, arg | arg
-void 
+Expr* 
 Parser::parse_arg_list(){
     parse_argument();
     while(matchif(token_comma)) parse_argument();
 }
 
-void
+Expr*
 Parser::parse_argument(){
     parse_primary_expr();
 }
 
 // literal | id | ( expr )
-void 
+Expr*
 Parser::parse_primary_expr(){
     // lookahead returns the token name
     switch(lookahead()){
@@ -282,7 +282,7 @@ Parser::parse_primary_expr(){
 
 // block | if | while | break | continue | return
 // | decl | expr
-void 
+Stmt* 
 Parser::parse_stmt(){
     Token token = peek();
     if(token.get_name() == token_left_brace) return parse_block_stmt();
@@ -315,7 +315,7 @@ Parser::parse_stmt(){
 }
 
 // { stmtseq }
-void
+Stmt*
 Parser::parse_block_stmt(){
     match(token_left_brace);
     parse_stmtseq();
@@ -323,7 +323,7 @@ Parser::parse_block_stmt(){
 }
 
 // break ; | continue ;
-void
+Stmt*
 Parser::parse_breaking_stmt(){
     Token token = peek();
     assert(token.get_name() == token_keywords);
@@ -340,7 +340,7 @@ Parser::parse_breaking_stmt(){
 }
 
 // if ( expr ) stmt | if ( expr ) stmt else stmt
-void 
+Stmt* 
 Parser::parse_if_stmt(){
     assert(peek().get_name() == token_keywords);
     accept();
@@ -353,7 +353,7 @@ Parser::parse_if_stmt(){
 }
 
 // while ( expr ) stmt
-void 
+Stmt* 
 Parser::parse_while_stmt(){
     assert(peek().get_name() == token_keywords);
     accept();
@@ -364,14 +364,14 @@ Parser::parse_while_stmt(){
 }
 
 // stmtseq stmt | stmt
-void
+Stmt*
 Parser::parse_stmtseq(){
     while(lookahead() != token_right_brace) parse_stmt();
 
 }
 
 // return expr ; | return ;
-void 
+Stmt* 
 Parser::parse_return_stmt(){
     assert(peek().get_name() == token_keywords);
     accept();
@@ -384,7 +384,7 @@ Parser::parse_return_stmt(){
 }
 
 // func decl | local decl
-void 
+Decl* 
 Parser::parse_decl(){
     switch(lookahead()){
         case key_def:{
@@ -403,13 +403,13 @@ Parser::parse_decl(){
 }
 
 // obj def
-void 
+Decl* 
 Parser::parse_local_decl(){
     parse_obj_def();
 }
 
 // var def | const def | value def
-void 
+Decl* 
 Parser::parse_obj_def(){
     Token token = peek();
     // this may not work. this may need to be token.get_sym_attr().get();
@@ -428,7 +428,7 @@ Parser::parse_obj_def(){
 }
 
 // var ident : type ; | var ident : type = expr
-void 
+Decl* 
 Parser::parse_var_def(){
     assert(peek().get_name() == token_keywords);
     accept();
@@ -449,7 +449,7 @@ Parser::parse_var_def(){
 }
 
 // let ident : type = expr ;
-void 
+Decl* 
 Parser::parse_const_def(){
     assert(peek().get_name() == token_keywords);
     accept();
@@ -462,7 +462,7 @@ Parser::parse_const_def(){
 }
 
 // def ident : type = expr ;
-void 
+Decl* 
 Parser::parse_val_def(){
       assert(peek().get_name() == token_keywords);
       accept();
@@ -476,7 +476,7 @@ Parser::parse_val_def(){
 }
 
 // def ident ( param list? ) -> type block stmt
-void 
+Decl* 
 Parser::parse_func_def(){
     assert(peek().get_name() == token_keywords);
     accept();
@@ -488,14 +488,14 @@ Parser::parse_func_def(){
 }
 
 // param list, param | param
-void
+Decl*
 Parser::parse_param_list(){
     parse_parameter();
     while(match(token_comma)) parse_parameter();
 }
 
 // ident : type
-void
+Decl*
 Parser::parse_parameter(){
     matchif(token_identifier);
     match(token_colon);
@@ -503,7 +503,7 @@ Parser::parse_parameter(){
 }
 
 // declseq decl | decl
-void 
+Decl* 
 Parser::parse_decl_seq(){
     while(!token_que.empty()) parse_decl();
 }
